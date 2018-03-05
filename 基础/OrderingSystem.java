b46bd62b6ae1b9cf8d9ba10c97933ef5c2a99fea
csh <15797713473@qq.com>
2018-03-05 21:27:57 +0800

    基础

---
+++ 基础/OrderingSystem.java
@@ -0,0 +1,17 @@
+package date0711;
+
+/**
+ * 吃货订餐系统主类
+ * 非面向对象
+ * 
+ * @author CSH
+ * @date 2018年7月11日上午7:58:28
+ */
+public class OrderingSystem {
+
+	public static void main(String[] args) {
+		System.out.println("欢迎使用“吃货联盟订餐系统”");
+		System.out.println("********************");
+		OrderingUtil.startMenu();
+	}
+}
