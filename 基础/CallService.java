03bee5db886d5dfde2c89a4a61f4f613bae47939
csh <15797713473@qq.com>
2018-03-10 21:31:08 +0800

    基础

---
+++ 基础/CallService.java
@@ -0,0 +1,11 @@
+package date0712;
+
+/**
+ * CallService接口
+ * @author CSH
+ * @date 2018年7月11日下午8:31:50
+ */
+public interface CallService {
+
+	public void call(int minCount,MobileCard card);//拨打电话
+}
