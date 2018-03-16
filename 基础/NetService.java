a663b01ce1d952d99c47fca38f57ffe871c06f2c
csh <15797713473@qq.com>
2018-03-16 21:39:48 +0800

    基础

---
+++ 基础/NetService.java
@@ -0,0 +1,11 @@
+package date0712;
+
+/**
+ * NetService接口
+ * @author CSH
+ * @date 2018年7月11日下午8:33:29
+ */
+public interface NetService {
+
+	public void netPlay(int flow,MobileCard card);//上网操作
+}
