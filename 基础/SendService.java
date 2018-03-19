9db5d7c28550d489f5e92152fe4059daa351f8d6
csh <15797713473@qq.com>
2018-03-19 21:41:50 +0800

    基础

---
+++ 基础/SendService.java
@@ -0,0 +1,11 @@
+package date0712;
+
+/**
+ * SendService接口
+ * @author CSH
+ * @date 2018年7月11日下午8:32:53
+ */
+public interface SendService {
+
+	public void send(int count,MobileCard card);//发送短信
+}
